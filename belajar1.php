d8d71bf92005e358600d68a2a64d1bf55dcc0d8a
restuaji88 <restuajibwi@gmail.com>
2022-09-09 15:42:10 +0700

    upload

---
+++ belajar1.php
@@ -0,0 +1,12 @@
+<?php
+$nim="E12345678";
+$nama="Budi";
+$prodi="Teknik Informatika";
+echo "Selamat Praktikum";
+echo "<br>";
+echo $nim;
+echo "<br>";
+echo $nama;
+echo "<br>";
+echo $prodi;
+?>
---
+++ operator2.php
@@ -0,0 +1,18 @@
+<?php
+$x=10;
+$y=6;
+echo ($x + $y);
+echo "<br>";
+echo ($x - $y);
+echo "<br>";
+echo ($x * $y);
+echo "<br>";
+echo ($x / $y);
+echo "<br>";
+echo ($x % $y);
+$a = "Hello";
+echo "<br>";
+$b = $a . " world!";
+echo $b;
+Echo $a.  "--" . $b. "ini string operator";
+?>
